bc2418533c61fb13defe6847282e5b80d4d259b6
Tiger-Frog-Games <97881265+Tiger-Frog-Games@users.noreply.github.com>
2025-09-02 11:34:32 -0400

    removed unused packages

--- app.js
+++ app.js
@@ -1,13 +1,11 @@
 var express     = require("express"),
     app         = express(),
     bodyParser  = require("body-parser"),
-    cookieParser = require("cookie-parser"),
-    LocalStrategy = require("passport-local"),
-    flash        = require("connect-flash"),
     methodOverride = require("method-override"),
     normalizePort=require('normalize-port');
 
 require('dotenv').config();
+
 //routes
 var indexRoute = require("./routes/Index"),
     blogRoute = require("./routes/Blog"),
@@ -20,28 +18,8 @@ app.use(bodyParser.urlencoded({extended: true}));
 app.set("view engine", "ejs");
 app.use(express.static(__dirname + "/public"));
 app.use(methodOverride('_method'));
-app.use(cookieParser('secret'));
-app.use(flash());
 //require moment
 app.locals.moment = require('moment');
-
-// PASSPORT CONFIGURATION
-app.use(require("express-session")({
-    secret: process.env.PASSPORT_SECRET,
-    resave: true,
-    rolling: true,
-    saveUninitialized: false,
-    cookie: {
-        secure: false,
-        maxAge: 3600000 //1 hour
-    }
-}));
-
-app.use(function(req, res, next){
-   res.locals.success = req.flash('success');
-   res.locals.error = req.flash('error');
-   next();
-});
 
 app.use("/",indexRoute);
 app.use("/Blog",blogRoute);
